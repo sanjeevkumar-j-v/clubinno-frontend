209b511f90ab6d0be4aa243f088b7a06b7f88fdb
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2021-07-15 23:03:49 +0530

    Include assets

---
+++ src/assets/js/profile.js
@@ -0,0 +1,8 @@
+VanillaTilt.init(document.querySelectorAll(".js-tilt"), {
+  max: 30,
+  speed: 600,
+  scale: 1,
+  transition: true,
+  easing: "cubic-bezier(.03,.98,.52,.99)",
+  perspective: 600,
+});
